a73940ba7132036d7b13613e42076b2b0a3b5559
Rafael <rafaelflores933@gmail.com>
2023-07-25 19:32:02 -0300

    finalizado exercicio 8

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,12 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if (idade < 12 || possuiPatologia == true || altura < 150) {
+    console.log("ACESSO NEGADO")
+}
+else if (idade < 18 || ehEstudante == true) {
+    console.log("R$ 10,00")
+}
+else {
+    console.log("R$ 20,00")
+}
