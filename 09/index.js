e381a523738723f2d11013a048b2a3c6ca8dcfef
Rafael <rafaelflores933@gmail.com>
2023-07-25 19:35:35 -0300

    finalizado exercicio 9

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,16 @@
 const nota = 8.5;
+if (nota >= 9) {
+    console.log("O estudando obteve conceito A")
+}
+else if (nota >= 8 && nota <= 8, 9) {
+    console.log("O estudando obteve conceito B")
+}
+else if (nota >= 6 && nota <= 7, 9) {
+    console.log("O estudando obteve conceito C")
+}
+else if (nota >= 4 && nota <= 5, 9) {
+    console.log("O estudando obteve conceito D")
+}
+else {
+    console.log("O estudando obteve conceito E")
+}
