ed257607278faa3154132b9218fd20709f5b091f
Rafael <rafaelflores933@gmail.com>
2023-07-25 18:41:15 -0300

    finalizado exercicio 1

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,25 @@
 const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
-//seu código aqui
+if (jogada1 == "pedra" && jogada2 == "tesoura") {
+    console.log("pedra")
+}
+else if (jogada1 == "papel" && jogada2 == "tesoura") {
+    console.log("tesoura")
+}
+else if (jogada1 == "tesoura" && jogada2 == "pedra") {
+    console.log("pedra")
+}
+else if (jogada2 == "pedra" && jogada1 == "tesoura") {
+    console.log("pedra")
+}
+else if (jogada2 == "papel" && jogada1 == "tesoura") {
+    console.log("tesoura")
+}
+else if (jogada2 == "pedra" && jogada == "tesoura") {
+    console.log("tesoura")
+}
+
+else {
+    console.log("empate")
+}
