44ca6cb30ec11d46fc600d7861742d18f8c9481c
Rafael <rafaelflores933@gmail.com>
2023-07-25 20:01:02 -0300

    finalizado primeira parte do exercicio 11

--- 11/index.js
+++ 11/index.js
@@ -6,3 +6,7 @@ const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+if (mesesDecorridos < 60 && rendaMensalEmCentavos > 200000 && totalJaPagoPeloAluno < 1800000) {
+    console.log(`O valor da parcela desse mês é R$ ${18 * (rendaMensalEmCentavos / 100) / 100} `)
+}
