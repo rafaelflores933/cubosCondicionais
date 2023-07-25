d3818c849bbe7518ba0562009ae4af5ca3ce35f2
Rafael <rafaelflores933@gmail.com>
2023-07-25 20:23:22 -0300

    finalizado exercicio 15

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,16 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if (primeiroNome && sobrenome && apelido) {
+    console.log(apelido)
+}
+else if (primeiroNome && sobrenome) {
+    console.log(primeiroNome + " " + sobrenome)
+}
+else if (primeiroNome && apelido) {
+    console.log(apelido)
+}
+else if (primeiroNome) {
+    console.log(primeiroNome)
+}
+
