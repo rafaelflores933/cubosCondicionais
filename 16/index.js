c4e8264d611212348e1ddb1028e9cbf17585f3a9
Rafael <rafaelflores933@gmail.com>
2023-07-25 20:26:32 -0300

    finalizado exercicio 16

--- 16/index.js
+++ 16/index.js
@@ -3,3 +3,9 @@ const idadeDoAluno = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+if (idadeDoAluno < 18 && possuiResponsavel == false) {
+    console.log("Não é possível fazer a rematrícula")
+}
+else {
+    console.log("Rematrícula realizada com sucesso")
+}
