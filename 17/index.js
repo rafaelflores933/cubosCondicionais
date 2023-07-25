ef3f33c19a8ed0073bc7f5a2774f401e23df472e
Rafael <rafaelflores933@gmail.com>
2023-07-25 20:52:25 -0300

    finalizado exercicio 17

--- 17/index.js
+++ 17/index.js
@@ -6,3 +6,8 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+let vlProduto = valorDoProduto / 100
+let valorParcela = vlProduto / quantidadeDoParcelamento
+let vlRestante = vlProduto - valorPago
+let parcelaRestante = (vlRestante / valorParcela)
+console.log(`Restam ${parcelaRestante} parcelas de R$${valorParcela}`)
